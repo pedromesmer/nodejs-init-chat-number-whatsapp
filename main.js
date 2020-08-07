3aca7de795e90b42453b7c6eefcc642563eb9e86
pedromesmer <pedro_mesmer@hotmail.com>
2020-08-06 21:56:54 -0300

    ddd dinamico e mascara de telefone ok

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const {app, BrowserWindow} = require('electron')
+const open = require('open')
+
 
 function createWindow () {
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 285, // 285
+        height: 520, // 520
         webPreferences: {
             nodeIntegration: true
         }
     })
 
-    win.loadFile('index.html')
+    win.loadFile('frontend/index.html')
 }
 
 app.whenReady().then(createWindow)
+// link: https://api.whatsapp.com/send?phone=seunumerodetelefone&text=sua%20mensagem
+
+
+function configNumber(element) {
+    number = element.value
+    number = number.replace(/\D/g,"")
+    console.log(number)
+    element.value = number
+}
+
+function send(number, text, navigator) {
+    console.log('vai dar certo')
+}
+
